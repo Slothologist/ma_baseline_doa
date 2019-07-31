e046edd8e850582bd210d5bdf8c0fe4444b9bd0b
rfeldhans <rfeldhans@techfak.uni-bielefeld.de>
2019-07-31 16:51:50 +0200

    bunch of bugfixes: fixed audio output; added sslinfo output; kinda probably fixed fixed microphone locations in pepper config; kinda probably fixed actual pyroomacustics calls; fixed config reading of microphone geometry

--- esiaf_doa/doa_wrapper.py
+++ esiaf_doa/doa_wrapper.py
@@ -2,20 +2,19 @@ import pyroomacoustics as pra
 import numpy as np
 
 # algorithms parameters
-SNR = 0.    # signal-to-noise ratio
-c = 343.    # speed of sound
+SNR = 3.    # signal-to-noise ratio
 fs = 16000  # sampling frequency
 nfft = 256  # FFT size
-freq_bins = np.arange(5, 60)  # FFT bins to use for estimation
+freq_range = [300, 3500]  # frequency range over which to perform doa
 
 
 def create_mic_config(config):
     mic_array = config['mic_array']
     xes = []
     ys = []
-    for mic in mic_array:
-        xes.append(mic_array[mic]['x'])
-        ys.append(mic_array[mic]['y'])
+    for channel in ['channel_' + str(x) for x in range(1, 5)]:
+        xes.append(mic_array[channel]['x'])
+        ys.append(mic_array[channel]['y'])
 
     complete = np.array([xes, ys])
     return complete
@@ -24,15 +23,24 @@ def create_mic_config(config):
 class DOA:
 
     def __init__(self, config):
-        algo_name = config['algorithm']
+        self.algo_name = config['algorithm']
         self.mic_config = create_mic_config(config)
-        self.doa = pra.doa.algorithms[algo_name](self.mic_config, fs, nfft, c=c, max_four=4)
+        self.doa = pra.doa.algorithms[self.algo_name](self.mic_config, fs, nfft, num_src=1, max_four=4)
+
+    @staticmethod
+    def _preprocessing(audio):
+        X = np.array([pra.stft(signal, nfft, nfft // 2, transform=np.fft.rfft).T for signal in audio])
+        return X
 
     def process_audio(self, audio):
+        # bring audio to frequency domain
+        # freq = self._calc_freq_domain(audio)
+        X = self._preprocessing(audio)
+
         # calculate doa
-        self.doa.locate_sources(audio, freq_bins=freq_bins)
+        self.doa.locate_sources(X, freq_range=freq_range)
 
         # calculate doa in degrees
         doa = self.doa.azimuth_recon / np.pi * 180.
 
-        return doa
+        return doa[0]
--- scripts/start_doa.py
+++ scripts/start_doa.py
@@ -3,7 +3,7 @@
 from esiaf_doa.doa_wrapper import DOA
 import pyesiaf
 import rospy
-from esiaf_ros.msg import RecordingTimeStamps, AugmentedAudio, SSLDir
+from esiaf_ros.msg import RecordingTimeStamps, AugmentedAudio, SSLDir, SSLInfo
 
 # config
 import yaml
@@ -38,12 +38,15 @@ if len(argv) < 2:
 path_to_config = argv[1]
 data = yaml.safe_load(open(path_to_config))
 
+rospy.loginfo('Adding SSLInfo publisher...')
+ssl_publisher = rospy.Publisher(nodename + '/' + 'SSL', SSLInfo, queue_size=10)
+
 rospy.loginfo('Creating direction of arrival instance...')
 
 wrapper = DOA(data)
 
 rospy.loginfo('Creating esiaf handler...')
-handler = pyesiaf.Esiaf_Handler('esiad_doa', pyesiaf.NodeDesignation.SSL, sys.argv)
+handler = pyesiaf.Esiaf_Handler(nodename, pyesiaf.NodeDesignation.SSL, sys.argv)
 
 rospy.loginfo('Setting up esiaf...')
 esiaf_format = pyesiaf.EsiafAudioFormat()
@@ -72,8 +75,8 @@ def input_callback(audio, timeStamps):
     msg_from_string(_recording_timestamps, timeStamps)
 
     # call dao wrapper
-    doa = wrapper.process_audio(audio)
-    rospy.loginfo('Current doa: ' + str(doa))
+    doa = wrapper.process_audio(audio.transpose())
+    rospy.loginfo('DOA from ' + str(_recording_timestamps.finish.secs) + ': ' + str(doa))
 
     # assemble output
     doa_esiaf = SSLDir()
@@ -81,13 +84,21 @@ def input_callback(audio, timeStamps):
     doa_esiaf.angleVertical = 0.0
     doa_esiaf.sourceId = 'no tracking'
 
-    doas = [doa_esiaf]
-    handler.set_ssl_dirs(doas)
+    doas = [msg_to_string(doa_esiaf)]
+    handler.set_ssl_dirs(data['esiaf_output_topic'], doas)
 
     # publish output
     handler.publish(data['esiaf_output_topic'],
                     audio,
-                    msg_to_string(timeStamps))
+                    timeStamps)
+
+    # assemble output
+    output = SSLInfo()
+    output.duration = _recording_timestamps
+    output.directions = [doa_esiaf]
+
+    ssl_publisher.publish(output)
+
 
 
 handler.add_input_topic(esiaf_audio_info, input_callback)
